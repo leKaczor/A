b8aaa39f5a72f5f2d59648b93d51c26563986c90
Sebastian Chojnacki <Sebastian.Chojnacki@365.aplitt.pl>
2022-01-25 21:57:47 +0100

    gui menu

---
+++ app/src/main/java/com/example/guiexample/uczenMenu.kt
@@ -0,0 +1,11 @@
+package com.example.guiexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class uczenMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uczen_menu)
+    }
+}
